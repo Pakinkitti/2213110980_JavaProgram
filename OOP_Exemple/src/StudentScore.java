9bab9b40d85ed2ab586ce34a0b3a611aaacf38a0
Pakinkitti <camp.ghs@gmail.com>
2022-12-23 14:36:31 +0700

    week07

--- OOP_Exemple/src/StudentScore.java
+++ OOP_Exemple/src/StudentScore.java
@@ -2,31 +2,11 @@ import java.util.*;
 
 public class StudentScore {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*
-		 * Scanner scan = new Scanner(System.in); Student[] std = new Student[3];
-		 * for(int i = 0 ; i<std.length;i++) { std [i] = new Student();
-		 * System.out.print("Input student name : "); std[i].setName(scan.next());
-		 * System.out.print("Input student score : "); std[i].setScore(scan.nextInt());
-		 * while(!std[i].checkscore()) {
-		 * System.out.print("Input student score, again : ");
-		 * std[i].setScore(scan.nextInt()); }// end of while System.out.println(); }
-		 * //end of for System.out.println();
-		 * System.out.println("LIST OF PASS STUDENT (>=50)");
-		 * System.out.println("-------------------------------------"); /*for(int i =
-		 * 0;i<std.length;i++) { if(std[i].isPass()) {
-		 * System.out.println(">> "+std[i].getName()+" ( "+std[i].getScore()+" )" ); }
-		 */
 
-		/*
-		 * 0for(Student STD : std) { if(STD.isPass()) {
-		 * System.out.println(">> "+STD.getName()+" ( "+STD.getScore()+" )" ); }
-		 * 
-		 * }
-		 */
 		Scanner scan = new Scanner(System.in);
 		System.out.print("How many student in classroom : ");
 		int size = scan.nextInt();
+		System.out.println();
 		Student[] std = new Student[size];
 		for (int i = 0; i < std.length; i++) {
 			std[i] = new Student();
@@ -38,14 +18,13 @@ public class StudentScore {
 				System.out.print("Input student score, again : ");
 				std[i].setScore(scan.nextInt());
 			}
+			System.out.println();
 			System.out.println("-------------------------------------");
 		}
-		for(Student STD : std){
-			if(STD.isPass()) {
-				System.out.println(">> "+STD.getName()+"get grade "+STD.findGrade(STD.getScore()) ); 
-		 }
-			
-		
+		for (Student STD : std) {
+			if (STD.isPass()) {
+				System.out.println(">> " + STD.getName() + "get grade " + STD.findGrade(STD.getScore()));
+			}
 		}
 	}
 
